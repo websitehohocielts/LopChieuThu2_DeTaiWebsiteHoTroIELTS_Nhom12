0ae24ccb815c2ed59fb0ed5190428f8e2d73f9e1
cong196 <vancongct.96@gmail.com>
2016-11-05 17:01:54 +0700

    Add files via upload

--- src/DangNhapServlet.java
+++ src/DangNhapServlet.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class DangNhapServlet
@@ -37,11 +39,26 @@ public class DangNhapServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String user = request.getParameter("user1");
-		String pass = request.getParameter("pass1");
+		String user = request.getParameter("user");
+		String pass = request.getParameter("pass");
+		
 		
 		if(connectDB.ConnDB(user, pass)){
 			
+			HttpSession userdn = request.getSession();
+			userdn.setAttribute("username", user);
+			userdn.setAttribute("hoten", connectDB.hoten(user));
+			
+			User uss = new User();
+			uss = connectDB.ThongTin(user);
+			
+			userdn.setAttribute("username1", uss.getUsername());
+			userdn.setAttribute("hoten1", uss.getHoten());
+			userdn.setAttribute("sodienthoai", uss.getSodienthoai());
+			userdn.setAttribute("email", uss.getEmail());
+
+			String tb = "";
+			userdn.setAttribute("Thongbao", tb);
 			if(user.equals("admin")){
 				RequestDispatcher rd = request.getRequestDispatcher("Trang Quan Ly Admin.jsp");
 				rd.forward(request, response);
@@ -53,8 +70,16 @@ public class DangNhapServlet extends HttpServlet {
 		}
 		else
 		{
-			RequestDispatcher rd = request.getRequestDispatcher("111.jsp");
-			rd.forward(request, response);
+			
+			HttpSession Loi = request.getSession();
+			String tb = "Sai mật khẩu, đăng ký hoặc đăng nhập lại !";
+			Loi.setAttribute("Thongbao", tb);
+	RequestDispatcher rd = request.getRequestDispatcher("DangKy.jsp");
+	rd.forward(request, response);
+			
+			
+			
+			
 		}
 	}
 
--- src/connectDB.java
+++ src/connectDB.java
@@ -3,11 +3,13 @@
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 
 public class connectDB {
+	
 	public static boolean ConnDB(String username,String password)
 	{
 		try {
@@ -30,7 +32,93 @@ public class connectDB {
 			
 			e.printStackTrace();
 		}
+		try {
+			stm.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			cnn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return tmp;
+	}
+	
+	public static String hoten(String username)
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		Connection cnn = null;
+		Statement stm = null;
+		ResultSet rs = null;	
+		String name = null;
+		try {
+			cnn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/ieltsonline", "root", "cong12");
+			stm = (Statement) cnn.createStatement();
+			rs = stm.executeQuery("select hoten from ieltsonline.user where username = '"+username+"'");
+			while(rs.next()){
+				name = rs.getString("hoten");
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return name;
+	}
+	
+	public static User ThongTin(String username){
+		
+		Connection cnn = null;            
+        Statement stm = null;     
+       String sql = null;
+       
+       try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+       try {
+			cnn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/ieltsonline", "root", "cong12");
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+       try {
+			stm = (Statement) cnn.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+      User user = new User();
+      try {
+      	
+      	ResultSet rs = stm.executeQuery("SELECT username,hoten,sodienthoai,email,trangthai FROM ieltsonline.user where username = '"+username+"'");
+      
+          while(rs.next()) {	
+          
+          	user.setUsername(rs.getString("username"));
+          	user.setHoten(rs.getString("hoten"));
+              user.setSodienthoai(rs.getInt("sodienthoai"));
+              user.setEmail(rs.getString("email"));
+          	user.setTrangthai(rs.getBoolean("trangthai"));
+          	
+          }
+      } catch (SQLException e) {
+          e.printStackTrace();
+      }
+
+      return user;
 	}
 
 }
