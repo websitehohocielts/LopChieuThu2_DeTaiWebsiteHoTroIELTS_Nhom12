76b4ffd7fa1894ffe488445d0654542d48e6e141
cong196 <vancongct.96@gmail.com>
2016-11-07 16:24:05 +0700

    dang nhap dung bean

---
+++ src/mypack/DangKyBean.java
@@ -0,0 +1,42 @@
+package mypack;
+
+import java.math.BigInteger;
+
+public class DangKyBean {
+	private String userName;
+	private String password;
+	private String hoTen;
+	private BigInteger soDienThoai;
+	private String email;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getHoTen() {
+		return hoTen;
+	}
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+	public BigInteger getSoDienThoai() {
+		return soDienThoai;
+	}
+	public void setSoDienThoai(BigInteger soDienThoai) {
+		this.soDienThoai = soDienThoai;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ src/mypack/UserDN.java
@@ -0,0 +1,19 @@
+package mypack;
+
+public class UserDN {
+	String userName;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String password;
+
+}
